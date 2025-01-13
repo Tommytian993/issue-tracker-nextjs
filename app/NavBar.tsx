9bbe24a6e72f034de63af4f6ac2957937e23cdd6
Jerrywang993 <wang.tianye2@northeastern.edu>
2025-01-13 11:45:17 -0500

    feat(NavBar): add bug icon as logo and enhance nav styling

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,10 +1,13 @@
 import Link from "next/link";
 import React from "react";
+import { AiFillBug } from "react-icons/ai";
 
 const NavBar = () => {
   return (
-    <nav className="flex space-x-6 border-b">
-      <Link href="/">Logo</Link>
+    <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
+      <Link href="/">
+        <AiFillBug />
+      </Link>
       <ul className="flex space-x-6">
         <li>
           <Link href="/">Dashboard</Link>
