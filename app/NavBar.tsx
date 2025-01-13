cf8236de7a7ce81214c0c26488fe2b9540aec582
Jerrywang993 <wang.tianye2@northeastern.edu>
2025-01-13 12:10:56 -0500

    refactor(NavBar): use links array for navigation items

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -3,6 +3,10 @@ import React from "react";
 import { AiFillBug } from "react-icons/ai";
 
 const NavBar = () => {
+  const links = [
+    { label: "Dashboard", href: "/" },
+    { label: "Issues", href: "/issues" },
+  ];
   return (
     <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
       <Link href="/">
