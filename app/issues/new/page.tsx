f3a6bb43c1fc50195bfc9eb643b2cc3eb0f53521
Jerrywang993 <wang.tianye2@northeastern.edu>
2025-01-27 12:46:55 -0500

    feat(issues): add NewIssuePage with Radix UI TextField for title input

---
+++ app/issues/new/page.tsx
@@ -0,0 +1,14 @@
+import { TextField } from "@radix-ui/themes";
+import React from "react";
+
+const NewIssuePage = () => {
+  return (
+    <div>
+      <TextField.Root>
+        <TextField.Input placeholder="Title" />
+      </TextField.Root>
+    </div>
+  );
+};
+
+export default NewIssuePage;
