c9b3a72dd0998542be14ab65c013ef18dfedb5a3
Jerrywang993 <wang.tianye2@northeastern.edu>
2025-01-28 08:06:43 -0500

    style: add spacings between issue input fields

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 const NewIssuePage = () => {
   return (
-    <div className="max-w-xl">
+    <div className="max-w-xl space-y-3">
       <TextField.Root>
         <TextField.Input placeholder="Title" />
       </TextField.Root>
