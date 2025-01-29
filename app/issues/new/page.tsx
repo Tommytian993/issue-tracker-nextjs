cfd935268e9dad182c9e5a1e05c8f4df19b4da09
Jerrywang993 <wang.tianye2@northeastern.edu>
2025-01-29 07:01:24 -0500

    chore(issues): add IssueForm interface for type safety in new issue form

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,10 +1,19 @@
 "use client";
-import { TextArea, TextField } from "@radix-ui/themes";
+import { useForm } from "react-hook-form";
+import { Button, TextField } from "@radix-ui/themes";
 import React from "react";
 import SimpleMdeReact from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
 
+// this is the type of the form data
+interface IssueForm {
+  title: string;
+  description: string;
+}
+
 const NewIssuePage = () => {
+  const { register, handleSubmit } = useForm<IssueForm>();
+
   return (
     <div className="max-w-xl space-y-3">
       <TextField.Root>
